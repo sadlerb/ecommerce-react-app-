780853c3ab22ef4b8d0533ca5870086a80c47d48
sadlerb <sadlerb22@gmail.com>
2022-08-18 11:57:44 -0500

    initilized dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -4,9 +4,14 @@ import { rootReducer } from "./root-reducer";
 import {persistStore,persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !=='production' && logger].filter(Boolean);
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-const composedEnhancers = compose(applyMiddleware(...middleWares))
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 const persistConfig ={
     key:'root',
     storage,
@@ -14,6 +19,7 @@ const persistConfig ={
 }
 
 const persistedReducer = persistReducer(persistConfig,rootReducer)
+
 
 
 export const store = createStore(persistedReducer,undefined,composedEnhancers);
